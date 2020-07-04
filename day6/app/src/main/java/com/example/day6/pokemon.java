b7efac9b6a33fa50a2124917dd6c2bdfd170db6a
noraalghareeb637 <66641305+noraalghareeb637@users.noreply.github.com>
2020-07-04 17:57:39 +0300

    done

---
+++ day6/app/src/main/java/com/example/day6/pokemon.java
@@ -0,0 +1,55 @@
+package com.example.day6;
+
+import android.media.Image;
+
+public class pokemon {
+    private String name ;
+    private int image ;
+    private int attack ;
+   private int defence ;
+   private int total ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+}
+
+
+
+}
